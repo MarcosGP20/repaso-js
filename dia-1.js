abddb4071ef30848e318456c9156728dcd0098fd
MarcosGP20 <marcospinto2003@gmail.com>
2024-12-03 18:11:01 -0300

    Di 1 terminado nashe

---
+++ dia-1.js
@@ -0,0 +1,28 @@
+// Objetivo: Entender la sintaxis básica y los tipos de datos.
+
+// Conceptos a cubrir:
+// Variables (var, let, const).
+// Tipos de datos (string, number, boolean, undefined, null, object, array).
+// Operadores básicos (aritméticos, de comparación y lógicos).
+// Ejercicio:
+// Declara una variable nombre y asígnale tu nombre.
+// Declara dos números y calcula su suma, resta, multiplicación y división.
+// Escribe un programa que determine si un número es par o impar.
+
+var viejardo = "viejardopolis declaration";
+let nashe = "Coasjnnac";
+const bobi = 3.14;
+
+nombre = "Marcos Pinto";
+a = 2;
+b = 6;
+
+suma = a + b;
+resta = a - b;
+multiplicacion = a * b;
+division = a / b;
+
+let numeraco = 18;
+es_par_o_impar = numeraco % 2 === 0 ? "par" : "impar";
+
+console.log(`El numero ${numeraco} es ${es_par_o_impar}`);
